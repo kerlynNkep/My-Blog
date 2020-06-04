2f6a65de08adba52a343dd3121f50c5fdbad5863
kerlynnkep <kerlynmanyi@gmail.com>
2020-06-04 07:09:28 +0100

    started working on adding tags but not done. Had some little issues

--- My_Blog/blog/models.py
+++ My_Blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -22,6 +23,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager()      #The default manager
     published = PublishedManager()  #Our custom manager
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
--- My_Blog/blog/views.py
+++ My_Blog/blog/views.py
@@ -4,12 +4,32 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
+from taggit.models import Tag
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 2
-    template_name = 'blog/post/list.html'
+#class PostListView(ListView):
+#    queryset = Post.published.all()
+#    context_object_name = 'posts'
+#    paginate_by = 2
+#    template_name = 'blog/post/list.html'
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    paginator = Paginator(object_list, 3)           # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag':tag})
 
 
 def post_share(request, post_id):
